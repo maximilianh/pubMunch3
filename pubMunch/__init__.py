70e577ae2dce8e4b551241e9bfbed280d4d37b80
Ethan Steinberg <ethan.steinberg@gmail.com>
2018-04-13 11:16:52 -0700

    Oops, minor fix

--- pubMunch/__init__.py
+++ pubMunch/__init__.py
@@ -2,7 +2,12 @@ import argparse
 
 from . import pubCrawlLib
 
-def download_pmid():
+def download_pmid(pmid):
+    meta = pubCrawlLib.getArticleMeta(pmid)
+    pdf_file = pubCrawlLib.crawlOneDoc(meta, doc_type='pdf')
+    return pdf_file
+
+def download_pmid_program():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('pmid', type=str, help='The pubmed id to download')
 
@@ -10,8 +15,7 @@ def download_pmid():
 
     print(args)
 
-    meta = pubCrawlLib.downloadPubmedMeta(args.pmid)
-    pdf_file = pubCrawlLib.crawlOneDoc(meta, doc_type='pdf')
+    pdf_file = download_pmid(args.pmid)
 
     with open('{}.pdf'.format(args.pmid), 'wb') as file:
         file.write(pdf_file)
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'download_pmid=pubMunch:download_pmid',
+            'download_pmid=pubMunch:download_pmid_program',
         ],
     },
 )
